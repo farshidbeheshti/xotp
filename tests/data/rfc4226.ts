7065ed0a280c49e3f7da645db5253f362d200bb3
Farshid Beheshti <Farshid.Beheshti@gmail.com>
2024-11-14 00:46:05 +0330

    HOTP generated tokens should be tested against official test vectors provided by RFC #4226! (#1)
    * testing against test vectors in RFC#4226
    
    * moving rfc#4226 test vectors out of the code

---
+++ tests/data/rfc4226.ts
@@ -0,0 +1,56 @@
+export const secretKey = "12345678901234567890";
+
+export const data = [
+  {
+    counter: 0,
+    hmacSha1Result: "cc93cf18508d94934c64b65d8ba7667fb7cde4b0",
+    hotp: "755224",
+  },
+  {
+    counter: 1,
+    hmacSha1Result: "75a48a19d4cbe100644e8ac1397eea747a2d33ab",
+    hotp: "287082",
+  },
+  {
+    counter: 2,
+    hmacSha1Result: "0bacb7fa082fef30782211938bc1c5e70416ff44",
+    hotp: "359152",
+  },
+  {
+    counter: 3,
+    hmacSha1Result: "66c28227d03a2d5529262ff016a1e6ef76557ece",
+    hotp: "969429",
+  },
+  {
+    counter: 4,
+    hmacSha1Result: "a904c900a64b35909874b33e61c5938a8e15ed1c",
+    hotp: "338314",
+  },
+  {
+    counter: 5,
+    hmacSha1Result: "a37e783d7b7233c083d4f62926c7a25f238d0316",
+    hotp: "254676",
+  },
+  {
+    counter: 6,
+    hmacSha1Result: "bc9cd28561042c83f219324d3c607256c03272ae",
+    hotp: "287922",
+  },
+  {
+    counter: 7,
+    hmacSha1Result: "a4fb960c0bc06e1eabb804e5b397cdc4b45596fa",
+    hotp: "162583",
+  },
+  {
+    counter: 8,
+    hmacSha1Result: "1b3c89f65e6c9e883012052823443f048b4332db",
+    hotp: "399871",
+  },
+  {
+    counter: 9,
+    hmacSha1Result: "1637409809a679dc698207310c8c7fc07290d9e5",
+    hotp: "520489",
+  },
+];
+
+const formattedHeader = ``;
