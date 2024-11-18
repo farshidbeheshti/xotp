91a7c084bf1bd2593ca31a38c5fe899b0cf03f27
farshidbeheshti <farshid.beheshti@gmail.com>
2024-11-19 01:13:04 +0330

    feat: implementation of totp generation and validation functions

--- src/totp.ts
+++ src/totp.ts
@@ -1,65 +1,83 @@
 import { TOTPOptions } from "./types/";
+import { HOTP } from "./hotp";
 
 class TOTP {
   algorithm = this.defaults.algorithm;
-  duration = this.defaults.duration;
   digits = this.defaults.digits;
   window = this.defaults.window;
-  timestamp = this.defaults.timestamp;
+  duration = this.defaults.duration;
+  encoding = this.defaults.encoding;
+  #hotp: HOTP;
   constructor({
     algorithm = this.defaults.algorithm,
     window = this.defaults.window,
     duration = this.defaults.duration,
     digits = this.defaults.digits,
+    encoding = this.defaults.encoding,
   }: Partial<TOTPOptions> = {}) {
-    this.digits = digits;
     this.algorithm = algorithm;
+    this.digits = digits;
     this.window = window;
     this.duration = duration;
+    this.encoding = encoding;
+    this.#hotp = new HOTP({ algorithm, window, digits, encoding });
   }
-
   get defaults(): Readonly<TOTPOptions> {
     return Object.freeze<TOTPOptions>({
       algorithm: "sha1",
       duration: 30,
       digits: 6,
       window: 1,
-      timestamp: Date.now(),
+      encoding: "ascii",
     });
   }
 
   generate({
-    secretKey,
+    secret,
     timestamp = Date.now(),
-    duration = this.duration || this.defaults.duration,
+    duration = this.duration,
   }: {
-    timestamp: number;
-    secretKey: string;
+    secret: string;
+    timestamp?: number;
     duration?: number;
-  }) {}
+  }) {
+    return this.#hotp.generate({
+      secret,
+      counter: (timestamp / 1000 / duration) | 0,
+    });
+  }
 
   validate({
     token,
     secret,
     timestamp = Date.now(),
-    duration = this.duration || this.defaults.duration,
+    duration = this.duration,
+    window = this.window,
   }: {
-    duration: number;
-    timestamp: number;
-    secret: string;
     token: string;
-  }) {}
+    secret: string;
+    timestamp?: number;
+    duration?: number;
+    window?: number;
+  }): boolean {
+    return this.#hotp.validate({
+      token,
+      secret,
+      counter: (timestamp / 1000 / duration) | 0,
+      window: window,
+    });
+  }
 
   verifyDelta({
     token,
     secret,
     timestamp = Date.now(),
-    duration = this.duration || this.defaults.duration,
+    duration = this.duration,
   }: {
-    duration: number;
-    timestamp: number;
-    secret: string;
     token: string;
+    secret: string;
+    timestamp: number;
+    duration: number;
   }) {}
 
   keyUri({ issuer, label }: { issuer: string; label: string }) {}
--- src/types/totp.options.ts
+++ src/types/totp.options.ts
@@ -1,7 +1,9 @@
+import { Encoding } from "@src/encoding";
+
 export type TOTPOptions = {
   algorithm: "sha1" | "sha256" | "sha512";
   duration: number;
   digits: number;
   window: number;
-  timestamp: number;
+  encoding: Encoding;
 };
