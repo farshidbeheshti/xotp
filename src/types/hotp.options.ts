a8c2b70fd2867383e4c51619e092add844f6b600
farshidbeheshti <farshid.beheshti@gmail.com>
2024-11-19 00:51:14 +0330

    feat: user is now able to use secret keys encoded other than ascii encoding.

--- src/types/hotp.options.ts
+++ src/types/hotp.options.ts
@@ -1,6 +1,9 @@
+import { Encoding } from "@src/encoding";
+
 export type HOTPOptions = {
   algorithm: "sha1" | "sha256" | "sha512";
   counter: number;
   digits: number;
   window: number;
+  encoding: Encoding;
 };
