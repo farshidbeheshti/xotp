895e0504375cbe9662052cf9c3f900d71e7dfec3
farshidbeheshti <farshid.beheshti@gmail.com>
2024-11-26 20:52:37 +0330

    test: improve test cases

---
+++ tests/totp.test.ts
@@ -0,0 +1,54 @@
+import { time } from "console";
+import { TOTP } from "../src/";
+import { data, secret, duration } from "./data/rfc6238";
+import { randomNum } from "./util";
+
+describe("RFC #6238 Test Vectors", () => {
+  test.each(data)(
+    "TOTP - should generate token: $totp for UTC: $utc",
+    ({ timestamp, mode, totp: token }) => {
+      const generatedToken = new TOTP({
+        algorithm: mode,
+        digits: 8,
+      }).generate({
+        secret: secret[mode],
+        encoding: "ascii",
+        timestamp: timestamp * 1000,
+        duration,
+      });
+      expect(generatedToken).toEqual(token);
+    },
+  );
+
+  test.each(data)(
+    "TOTP - should find and validate token in a random search window within which the token is generated",
+    ({ timestamp, mode }) => {
+      const window = 10;
+      const rnd = randomNum(-window, window);
+      const encoding = "ascii";
+      const totp = new TOTP({
+        algorithm: mode,
+        digits: 8,
+        duration,
+        window,
+      });
+
+      let inWindow = duration * rnd;
+      if (inWindow <= -timestamp) inWindow = 0;
+
+      const token = totp.generate({
+        secret: secret[mode],
+        encoding,
+        timestamp: (timestamp + inWindow) * 1000,
+      });
+
+      const delta = totp.compare({
+        token: token,
+        secret: secret[mode],
+        encoding,
+        timestamp: timestamp * 1000,
+      });
+      expect(delta).toStrictEqual(inWindow / duration);
+    },
+  );
+});
