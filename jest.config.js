3fdcda2654c738581e7403bcaae00eb4b4eb08ac
farshidbeheshti <farshid.beheshti@gmail.com>
2024-12-11 00:58:07 +0330

    preparing to publish on npm registry

--- jest.config.js
+++ jest.config.js
@@ -1,6 +1,12 @@
+const { pathsToModuleNameMapper } = require("ts-jest");
+const { compilerOptions } = require("./tsconfig");
+
 module.exports = {
   testEnvironment: "node",
+  verbose: true,
   transform: {
     "^.+.tsx?$": ["ts-jest", {}],
   },
+  moduleNameMapper: pathsToModuleNameMapper(compilerOptions.paths),
+  modulePaths: ["<rootDir>"],
 };
